b4dcbdf3cd4dfe28ac73fdaf31e74d6455948cd6
重信　大道寺 <shigenobu0603style@gmail.com>
2026-08-29 14:13:27 +0900

    Swap Yuzu sprite sheet to latest uploaded asset

--- yuzu-wakeup-assets.js
+++ yuzu-wakeup-assets.js
@@ -2,6 +2,6 @@
 // 画像の差し替えは原則このファイルだけを変更する。
 window.YUZU_WAKEUP_ASSETS = {
   background: "./assets/IMG_1360.jpeg",
-  spriteSheet: "./assets/973820A9-91C1-4C8E-A8CB-327579CD3400.png",
+  spriteSheet: "./assets/C02605D1-F059-4C11-88BA-308CA4AB7989.png",
   wakeResult: "./B89EE4D0-FA7F-4222-950E-193DA12C67F3.png"
 };
