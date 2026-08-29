ffe090b9b393860b4fb89fe2893f72576e376cc9
重信　大道寺 <shigenobu0603style@gmail.com>
2026-08-29 14:23:49 +0900

    Add second-sleep result image asset

--- yuzu-wakeup-assets.js
+++ yuzu-wakeup-assets.js
@@ -3,5 +3,6 @@
 window.YUZU_WAKEUP_ASSETS = {
   background: "./assets/IMG_1360.jpeg",
   spriteSheet: "./assets/C02605D1-F059-4C11-88BA-308CA4AB7989.png",
-  wakeResult: "./B89EE4D0-FA7F-4222-950E-193DA12C67F3.png"
+  wakeResult: "./B89EE4D0-FA7F-4222-950E-193DA12C67F3.png",
+  sleepResult: "./assets/24938A11-8677-46CC-BFC8-3774D80E3D44.png"
 };
