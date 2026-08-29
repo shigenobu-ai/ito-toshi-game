db292ea47b1adee51370d8365e8cbe3964b235fe
重信　大道寺 <shigenobu0603style@gmail.com>
2026-08-29 09:46:10 +0900

    Separate Yuzu Wake Up image asset configuration

---
+++ yuzu-wakeup-assets.js
@@ -0,0 +1,7 @@
+// ゆずちゃんウェイクアップ：画像設定
+// 画像の差し替えは原則このファイルだけを変更する。
+window.YUZU_WAKEUP_ASSETS = {
+  background: "./assets/IMG_1360.jpeg",
+  spriteSheet: "./assets/973820A9-91C1-4C8E-A8CB-327579CD3400.png",
+  wakeResult: "./B89EE4D0-FA7F-4222-950E-193DA12C67F3.png"
+};
